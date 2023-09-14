4d083f5a0aa09dc5af8cb4960fe24773843643f4
camilim <caami.liim@gmail.com>
2023-09-14 10:10:41 -0600

    putting my first c++ code on repository

---
+++ src/cplus.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <math>
+#include <fstream> //allows me to create, read, and write to files
+using namespace std;
+double df(double x, double y)
+{
+ double ans = x+y;
+ return ans;
+}
+int main()
+{
+ int n;
+ double x0, t0, x, t, dt; //any variables to use
+ // the variables equal to the values that I set
+ cout<<1<<0;cin>>x0>>t0; //initial values
+ cout<< ("params.dat")
+ cin>>x;
+ cout<< //also another thing for step width in (params.dat)? 
+ cin>>dt
+ while(x0<x)
+ {
+	x = 
