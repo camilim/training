2fa7eda2c28460a7fa27fac64bafb22c7866c519
camilim <caami.liim@gmail.com>
2023-09-19 10:10:16 -0600

    updating the code round 2

--- src/cplus.c
+++ src/cplus.c
@@ -1,23 +1,31 @@
 #include <iostream>
-#include <iomanip>
-#include <math>
+#include <vector> 
 #include <fstream> //allows me to create, read, and write to files
 using namespace std;
-double df(double x, double y)
-{
- double ans = x+y;
- return ans;
-}
+
 int main()
 {
- int n;
- double x0, t0, x, t, dt; //any variables to use
- // the variables equal to the values that I set
- cout<<1<<0;cin>>x0>>t0; //initial values
- cout<< ("params.dat")
- cin>>x;
- cout<< //also another thing for step width in (params.dat)? 
- cin>>dt
- while(x0<x)
- {
-	x = 
+ double x0=1, t, dt; //any variables to use
+//the variables equal to the values that I set
+ cout<<("params.dat");
+ cin>>t;
+//put name of width file
+ cout<<("params_w.dat");
+ cin>>dt;
+ int nt = static_cast<int>((9)/dt + 1);
+ std::vector<double> t(nt);
+ std::vector<double> x(nt);
+
+ t[0] = t[0]
+ x[0] = x0;
+ for (int i=0, i<nt -1, i++)
+ { 
+	x[i+1] = (1.0 - 3.0*dt)*x[i];
+	t[i+1] = t[i] + dt;
+ }
+for (int i=0; i < nt; i++)
+	{
+	std::cout << "t[" << i << "] = " << t[i] << ", x[" <<i << "] = " << x[i] << std::endl;
+	}
+ return 0;
+}
