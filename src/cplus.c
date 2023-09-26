a37c6f225729acbfd201f020f9f77e90450f2298
camilim <caami.liim@gmail.com>
2023-09-26 10:09:09 -0600

    me reading my file successfully

--- src/cplus.c
+++ src/cplus.c
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <vector> 
-#include <fstream> //allows me to create, read, and write to files
-using namespace std;
+#include <fstream>
+#include <cmath>
+using namespace std; 
+
+double x0 = 1;
+double t_beg = 0;
+double t_end = 9;
 
 int main()
 {
- double x0=1, t, dt; //any variables to use
-//the variables equal to the values that I set
- cout<<("params.dat");
- cin>>t;
+ double dt[7]; //any variables to use
+ ifstream inFile;
+ inFile.open("params.dat");
+ if (inFile.is_open())
+ {
+ 	while (inFile >> dt[0]) 
+	{ cout << dt[0] << endl;
+	}
+	inFile.close();
+ }
+ return 0;
+}
 //put name of width file
- cout<<("params_w.dat");
+ /*cout<<("params_w.dat");
  cin>>dt;
  int nt = static_cast<int>((9)/dt + 1);
  std::vector<double> t(nt);
@@ -28,4 +41,4 @@ for (int i=0; i < nt; i++)
 	std::cout << "t[" << i << "] = " << t[i] << ", x[" <<i << "] = " << x[i] << std::endl;
 	}
  return 0;
-}
+}*/
