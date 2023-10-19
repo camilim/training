1ff888ef3fb7880233ef04e0d657ed3320288bd6
camilim <caami.liim@gmail.com>
2023-10-19 11:31:35 -0600

    test commit

---
+++ src/plot.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#mybb0 = np.loadtxt('output0.dat', delimiter = ' ')
+#x0 = mybb0[:, 0]
+#t0 = mybb0[:, 1]
+#plt.figure()
+#plt.plot(x0,t0, 'o', label='graph 1')
+#plt.show()
+
+#mybb1 = np.loadtxt('output1.dat', delimiter = ' ')
+#x1 = mybb1[:, 0]
+#t1 = mybb1[:, 1]
+#plt.figure()
+#plt.plot(x1,t1, 'o', label='graph 2')
+#plt.show()
+
+#mybb2 = np.loadtxt('output2.dat', delimiter = ' ')
+#x2 = mybb2[:, 0]
+#t2 = mybb2[:, 1]
+#plt.figure()
+#plt.plot(x2,t2, 'o', label='graph 3')
+#plt.show()
+
+#mybb3 = np.loadtxt('output3.dat', delimiter = ' ')
+#x3 = mybb3[:, 0]
+#t3 = mybb3[:, 1]
+#plt.figure()
+#plt.plot(x3,t3, 'o', label='graph 4')
+#plt.show()
+
+#mybb4 = np.loadtxt('output4.dat', delimiter = ' ')
+#x4 = mybb4[:, 0]
+#t4 = mybb4[:, 1]
+#plt.figure()
+#plt.plot(x4,t4, 'o', label='graph 5')
+#plt.show()
+
+mybb5 = np.loadtxt('output5.dat', delimiter = ' ')
+x5 = mybb5[:, 0]
+t5 = mybb5[:, 1]
+plt.figure()
+plt.plot(x5,t5, 'o')
+plt.title("Graph 6")
+plt.show()
+
+#mybb6 = np.loadtxt('output6.dat', delimiter = ' ')
+#x6 = mybb6[:, 0]
+#t6 = mybb6[:, 1]
+#plt.figure()
+#plt.plot(x6,t6, 'o', label='graph 7')
+#plt.show()
